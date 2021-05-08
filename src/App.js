78c5e59d43bc85c09eeddf45cca015b9390b9f20
SergeiP <sergi@systtekinteractive.com>
2021-05-08 18:05:05 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>¡Hello!</h1>
+        <h2>
+          Systtek Interactive
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
